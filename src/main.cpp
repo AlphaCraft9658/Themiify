991a8e09b4951b6a90424283431523a9e0098e9f
Alpha-Craft <knolld00@gmail.com>
2024-05-23 16:22:59 +0000

    Mount mlc using libmocha and read Men.pack file from there

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <whb/log_udp.h>
 #include <whb/log_console.h>
 #include <sysapp/title.h>
+#include <mocha/mocha.h>
 #include "hips.hpp"
 #include <cstdint>
 #include <cstdio>
@@ -15,6 +16,7 @@ int error() {
     while (WHBProcIsRunning()) {
             WHBLogConsoleDraw();
     }
+    Mocha_UnmountFS("storage_mlc");
     WHBLogConsoleFree();
     WHBLogUdpDeinit();
     WHBProcShutdown();
@@ -26,6 +28,12 @@ int main(int argc, char **argv)
     WHBProcInit();
     WHBLogUdpInit();
     WHBLogConsoleInit();
+    Mocha_InitLibrary();
+
+    // Mount storage_mlc
+    // NOTE: storage_mlc is the system mlc_storage where the system menu as well as other system titles are located
+    //       it could be risky when not handled properly, because wrong commands may override important system files.
+    Mocha_MountFS("storage_mlc", "/dev/mlc01", "/vol/storage_mlc");
 
     uint64_t menuID = _SYSGetSystemApplicationTitleId(SYSTEM_APP_ID_WII_U_MENU);  // Retrieve the Wii U system menu titleID
     // Convert the uint64_t menuID into a char array
@@ -34,9 +42,9 @@ int main(int argc, char **argv)
 
     // Generate and store the path of the content directory of the Wii U system menu both for sdcafiine and the system
     std::string sdcafiineContentFolder = menuIDString;
-    sdcafiineContentFolder = "fs:/vol/external01/wiiu/sdcafiine/" + sdcafiineContentFolder.substr(0, 8) + "/" + sdcafiineContentFolder.substr(8, 8) + "/content/";
+    sdcafiineContentFolder = "fs:/vol/external01/wiiu/sdcafiine/" + sdcafiineContentFolder.substr(0, 8) + "/" + sdcafiineContentFolder.substr(8, 8) + "/content";
     std::string systemContentFolder = menuIDString;
-    systemContentFolder = "storage_mlc:/sys/title/" + systemContentFolder.substr(0, 8) + "/" + systemContentFolder.substr(8, 8) + "/content/";
+    systemContentFolder = "storage_mlc:/sys/title/" + systemContentFolder.substr(0, 8) + "/" + systemContentFolder.substr(8, 8) + "/content";
 
     // Print those directories and the menu titleID for debugging purposes
     WHBLogPrintf("Wii U system menu titleID: %016hhs", menuIDString);
@@ -46,18 +54,21 @@ int main(int argc, char **argv)
     
     // Retrieve the paths of the input file and the patch file
     // Lil disclaimer, fs:/vol/external01/ is the path to the root of the sd card
-    const char* inputPath = "fs:/vol/external01/patch_files/Men.pack";
-    const char* patchPath = "fs:/vol/external01/patch_files/Men.bps";
+    // const char* internalMenPath = (systemContentFolder + "/Common/Package/Men.pack").c_str();
+    // const char* inputPath = "fs:/vol/external01/patch_files/Men.pack";
+    // const char* inputPath = std::string(systemContentFolder + "/Common/Package/Men.pack").c_str();
+    std::string inputPath = systemContentFolder + "/Common/Package/Men.pack";
+    std::string patchPath = "fs:/vol/external01/patch_files/Men.bps";
 
     // Open the input file
-    std::FILE* inputFile = std::fopen(inputPath, "rb");
+    std::FILE* inputFile = std::fopen(inputPath.c_str(), "rb");
     if (!inputFile) {
         WHBLogPrintf("Failed to open input file: %s\n", inputPath);
         return error();
     }
 
     // Open the patch file
-    std::FILE* patchFile = std::fopen(patchPath, "rb");
+    std::FILE* patchFile = std::fopen(patchPath.c_str(), "rb");
     if (!patchFile) {
         WHBLogPrintf("Failed to open patch file: %s\n", patchPath);
         std::fclose(inputFile);
@@ -135,6 +146,7 @@ int main(int argc, char **argv)
         WHBLogConsoleDraw();
     }
 
+    Mocha_UnmountFS("storage_mlc");
     WHBLogConsoleFree();
     WHBLogUdpDeinit();
     WHBProcShutdown();
