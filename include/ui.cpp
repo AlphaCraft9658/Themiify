2df9a042e9f89fb8cdb2a2c2bbbda6e0489c24e5
Alpha-Craft <knolld00@gmail.com>
2024-08-06 14:39:19 +0000

    More experimental UI code Mostly structure thoughts at the moment

--- include/ui.cpp
+++ include/ui.cpp
@@ -1,10 +1,33 @@
 #include "ui.h"
 using namespace ui;
-#define SUBMENU_ENTRY (int) 0;
-#define SELECTION_ENTRY (int) 1;
+#define SUBMENU_ENTRY (int) 0
+#define SELECTION_ENTRY (int) 1
 
-Menu::Menu() {
+Menu::Menu(std::string header) : header(generate_header_string(header)) {
     this->submenus = std::make_unique<std::vector<Menu>>();
+}
+
+std::string Menu::generate_header_string(std::string header) {
+    std::string headerString;
+    for (int c=0; c < header.size() * 3; c++) {
+        headerString.append("=");
+    }
+    headerString.append("\n");
+    for (int c=0; c < header.size(); c++) {
+        headerString.append(" ");
+    }
+    headerString.append(header);
+    for (int c=0; c < header.size(); c++) {
+        headerString.append(" ");
+    }
+    headerString.append("\n");
+    for (int c=0; c < header.size() * 3; c++) {
+        headerString.append("=");
+    }
+    return headerString;
+}
+
+void Menu::update() {
 }
 
 Entry::Entry(std::string label="Empty Label", int type) {
@@ -12,6 +35,6 @@ Entry::Entry(std::string label="Empty Label", int type) {
     this->type = type;
 }
 
-SubmenuEntry::SubmenuEntry(std::string label) {
-    Entry(label, SUBMENU_ENTRY);
-}
+SubmenuEntry::SubmenuEntry(std::string label) : Entry(label, SUBMENU_ENTRY) {}
+
+SelectionEntry::SelectionEntry(std::string label) : Entry(label, SUBMENU_ENTRY) {}
--- include/ui.h
+++ include/ui.h
@@ -1,25 +1,42 @@
 #pragma once
 #include <vector>
 #include <memory>
+#include <whb/proc.h>
+#include <whb/log.h>
+#include <whb/log_udp.h>
+#include <whb/log_console.h>
 
 namespace ui {
     class Menu {
         private:
             std::unique_ptr<std::vector<Menu>> submenus;
+            std::string header;
+            std::string generate_header_string(std::string header);
         public:
-            Menu();
+            Menu(std::string header);
+            void input_event();
+            void update();
+            void open_submenu(int index);
     };
 
     class Entry {
         public:
             std::string label;
             Entry(std::string label, int type);
+            std::unique_ptr<std::vector<Entry>> subEntries;
         protected:
+            std::unique_ptr<Entry> parent;
+            int layer;
             int type;
     };
 
     class SubmenuEntry: public Entry {
         private:
             SubmenuEntry(std::string label);
+    };
+
+    class SelectionEntry: public Entry {
+        private:
+            SelectionEntry(std::string label);
     };
 }
