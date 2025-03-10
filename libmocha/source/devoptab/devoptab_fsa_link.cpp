f126d4f3563275d9fa0b5e43ff078b437552f309
Alpha-Craft <knolld00@gmail.com>
2025-03-10 17:51:01 +0000

    Remove leftover libmocha files

--- libmocha/source/devoptab/devoptab_fsa_link.cpp
+++
@@ -1,8 +0,0 @@
-#include "devoptab_fsa.h"
-
-int __fsa_link(struct _reent *r,
-               const char *existing,
-               const char *newLink) {
-    r->_errno = ENOSYS;
-    return -1;
-}
--- libmocha/source/devoptab/devoptab_fsa_utimes.cpp
+++
@@ -1,8 +0,0 @@
-#include "devoptab_fsa.h"
-
-int __fsa_utimes(struct _reent *r,
-                 const char *filename,
-                 const struct timeval times[2]) {
-    r->_errno = ENOSYS;
-    return -1;
-}
